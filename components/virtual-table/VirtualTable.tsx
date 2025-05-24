2286acd73a41d1c3d5fbe2767c898f545f523d6f
slow123top <huyuyang2016@gmail.com>
2025-05-24 16:01:48 +0800

    export components and install components

--- components/virtual-table/VirtualTable.tsx
+++ components/virtual-table/VirtualTable.tsx
@@ -1,8 +1,8 @@
-import { defineComponent, PropType } from 'vue'
-import { Column } from './type'
-import Header from './header/Header'
-import Body from './body/Body'
-import './table.css'
+import { defineComponent, PropType } from 'vue';
+import { Column } from './type';
+import Header from './header/Header';
+import Body from './body/Body';
+import './table.css';
 
 export default defineComponent({
   name: 'HVirtualTable',
@@ -10,12 +10,12 @@ export default defineComponent({
     data: { type: Object },
     columns: { type: Object as PropType<Array<Column>> },
   },
-  setup(props, { emit, slots }) {
+  setup() {
     return (
       <div>
         <Header></Header>
         <Body></Body>
       </div>
-    )
+    );
   },
-})
+});
