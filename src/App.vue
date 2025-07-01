0905b174d4db2d7dd451458ec8ec6d317c040830
huyuyang <huyuyang@inspur.com>
2025-07-01 08:52:32 +0800

    split components

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,12 @@
 <script setup lang="ts">
-import { cloneDeep } from 'lodash-es';
-import { reactive, ref } from 'vue';
+import EditableTable from './components/EditableTable.vue';
 
-const data = ref([
-  { id: 'AAA', name: '张三', age: 12 },
-  { id: 'BBB', name: '李四', age: 18 },
-  { id: 'CCC', name: '王五', age: 20 },
-]);
-const columns = ref([
-  { key: 'id', name: '标识' },
-  { key: 'name', name: '名称' },
-  { key: 'age', name: '年龄' },
-  // { key: 'operation', name: '操作' }
-])
-const showLineNumberVar = ref(false);
-const editingData = reactive({});
-function showLineNumber() {
-  showLineNumberVar.value = !showLineNumberVar.value;
-}
-function editCell(id: string) {
-  editingData[id] = cloneDeep(data.value.filter((dataItem: any) => dataItem.id === id)[0]);
-}
-
-
-
-function cancel(id: string) {
-  editingData[id] = null;
-}
-
-function save(id: string) {
-  const foundData = data.value.filter((dataItem: any) => dataItem.id === id)[0];
-  Object.keys(editingData[id]).forEach((columnKey: string) => {
-    foundData[columnKey] = editingData[id][columnKey];
-  });
-  cancel(id);
-}
 </script>
 
 <template>
   <div>
-    <button @click="showLineNumber">切换显示行号</button>
-    列模板:
-    <h-virtual-table :data="data">
-      <template #default="{ row }">
-        <h-table-column id="id" name="标识">
-          <input type="text" v-model="editingData[row.id].id" v-if="editingData[row.id]">
-          <div v-else @click="editCell(row.id)">{{ row['id'] }}</div>
-        </h-table-column>
-        <h-table-column id="name" name="名称">
-          {{ row['name'] }}
-        </h-table-column>
-        <h-table-column id="age" name="年龄">
-          {{ row['age'] }}
-        </h-table-column>
-        <h-table-column id="operation" name="操作">
-          <button @click="save(row.id)">
-            保存
-          </button>
-          <button @click="cancel(row.id)">
-            取消
-          </button>
-        </h-table-column>
-      </template>
-    </h-virtual-table>
+    <div>单元格编辑例子</div>
+    <EditableTable></EditableTable>
   </div>
 </template>
 
--- src/components/EditableTable.vue
+++ src/components/EditableTable.vue
@@ -10,7 +10,8 @@ const data = ref([
 const columns = ref([
     { key: 'id', name: '标识' },
     { key: 'name', name: '名称' },
-    { key: 'age', name: '年龄' }
+    { key: 'age', name: '年龄' },
+    // { key: 'operation', name: '操作' }
 ])
 const showLineNumberVar = ref(false);
 const editingData = reactive({});
@@ -19,6 +20,20 @@ function showLineNumber() {
 }
 function editCell(id: string) {
     editingData[id] = cloneDeep(data.value.filter((dataItem: any) => dataItem.id === id)[0]);
+}
+
+
+
+function cancel(id: string) {
+    editingData[id] = null;
+}
+
+function save(id: string) {
+    const foundData = data.value.filter((dataItem: any) => dataItem.id === id)[0];
+    Object.keys(editingData[id]).forEach((columnKey: string) => {
+        foundData[columnKey] = editingData[id][columnKey];
+    });
+    cancel(id);
 }
 </script>
 
@@ -37,6 +52,14 @@ function editCell(id: string) {
                 </h-table-column>
                 <h-table-column id="age" name="年龄">
                     {{ row['age'] }}
+                </h-table-column>
+                <h-table-column id="operation" name="操作">
+                    <button @click="save(row.id)">
+                        保存
+                    </button>
+                    <button @click="cancel(row.id)">
+                        取消
+                    </button>
                 </h-table-column>
             </template>
         </h-virtual-table>
